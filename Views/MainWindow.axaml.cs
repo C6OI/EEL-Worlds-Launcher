57b453a9a18f0a1cf94b87f149e2aa93b411e417
C6OI <C6OI@yandex.ru>
2022-09-08 16:41:27 +0500

    Started working on minecraft

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,20 +1,29 @@
 using System;
 using System.Diagnostics;
+using System.Net;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
+using CmlLib.Core;
+using CmlLib.Core.Auth;
 using EELauncher.Extensions;
 
 namespace EELauncher.Views {
     public partial class MainWindow : Window {
-        readonly IAssetLoader _assets = AvaloniaLocator.Current.GetService<IAssetLoader>()!;
-
+        readonly IAssetLoader _assets = AvaloniaLocator.Current.GetService<IAssetLoader>()!; 
+        readonly MinecraftPath _pathToMinecraft = new();
+        CMLauncher _launcher;
+        
         public MainWindow() {
+            _launcher = new CMLauncher(_pathToMinecraft);
+            
             InitializeComponent();
             ClientSize = new Size(960, 540);
+            ServicePointManager.DefaultConnectionLimit = 256;
         }
 
         public void OnInitialized(object? sender, EventArgs e) {
@@ -31,17 +40,17 @@ namespace EELauncher.Views {
             Background = bg;
         }
         
-        public void NewsShortBlock_OnInitialized(object? sender, EventArgs e) {
+        public void NewsDescription_OnInitialized(object? sender, EventArgs e) {
             TextBlock textBlock = (TextBlock)sender!;
             
             textBlock.Text = "Раздача на спувне бесплатные шалкеры awawa!!\n" +
                              "Вчера я съел угря\n" +
                              "Что такое galnet и где кошкодевочки\n";
+
+            textBlock.Text = _launcher.MinecraftPath.BasePath;
         }
         
         void NewsImage_OnPointerPressed(object? sender, PointerPressedEventArgs e) {
-            Image image = (Image)sender!;
-
             "http://eelworlds.ml/news/coming-soon".OpenUrl();
         }
         
@@ -62,6 +71,15 @@ namespace EELauncher.Views {
             };
 
             button.Content = leaveButtonImage;
+        }
+
+        async void PlayButton_OnClick(object? sender, RoutedEventArgs e) {
+            Process process = await _launcher.CreateProcessAsync("1.19.2", new MLaunchOption {
+                MaximumRamMb = 2048,
+                Session = MSession.GetOfflineSession(NicknameField.Text),
+            });
+
+            process.Start();
         }
     }
 }
